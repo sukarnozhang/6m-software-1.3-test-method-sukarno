21b1ec9b823cd32ba254018b628223392de7d61e
sukarnozhang <78150905+sukarnozhang@users.noreply.github.com>
2023-08-15 20:10:24 +0800

    first attempt

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -10,6 +10,17 @@
 const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PROD"
 let databaseCredential = "devuser:password";
 
-// Task: Add code here
+
+switch (env) {
+  case "DEV":
+    databaseCredential = "devuser:password";
+    break;
+  case "STAGE":
+    databaseCredential = "stageuser:password";
+    break;
+    case "PROD":
+    databaseCredential = "produser:password";
+    break;
+}
 
 console.log(`Database credential for environment ${env} is ${databaseCredential}`);
